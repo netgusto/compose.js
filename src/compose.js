79b1ce034ad4b8b93628b1a15a6c6d85132f5f5e
Jérôme Schneider / Net Gusto <contact@netgusto.com>
2016-02-29 23:16:35 +0100

    Better handling of expectations

--- src/compose.js
+++ src/compose.js
@@ -23,28 +23,34 @@ export default function compose(...factories) {
     }, {})));
 
     const res = Object.assign({}, basefactoryobject, {
-        init: function() {
-            factobjects.map(factory => 'init' in factory ? factory.init.apply(this) : null);
+        init: function(buildprops) {
+            factobjects.map(factory => 'init' in factory ? factory.init.apply(this, [buildprops]) : null);
         },
         expects: Object.assign({}, ...factobjects.map(factory => 'expects' in factory ? factory.expects : {})),
         props: merge.recursive({}, ...factobjects.map(factory => 'props' in factory ? factory.props : {})),
         methods: Object.assign({}, ...factobjects.map(factory => 'methods' in factory ? factory.methods : {})),
         create: function(buildprops = {}) {
 
-            const compiled = Object.assign({}, clone(this.props, false), this.methods, buildprops);
+            const compiled = Object.assign({}, clone(this.props, false), this.methods);
 
             Object.keys(this.expects).map(expectationname => {
-                if(!(expectationname in compiled)) {
+                if(!(expectationname in compiled) && !(expectationname in buildprops)) {
                     throw new Error('Failed expectation for ' + expectationname + '; property is missing.');
                 }
                 let expectationtype = this.expects[expectationname];
 
-                if(compiled[expectationname].constructor !== expectationtype) {
-                    throw new Error('Failed expectation for ' + expectationname + '; type mismatch; expected ' + expectationtype.name + ', got ' + compiled[expectationname].constructor.name);
+                if(expectationname in buildprops) {
+                    if(buildprops[expectationname].constructor !== expectationtype && !(buildprops[expectationname] instanceof expectationtype)) {
+                        throw new Error('Failed expectation for ' + expectationname + '; type mismatch; expected ' + expectationtype.name + '; got ' + buildprops[expectationname].constructor.name);
+                    }
+                } else if(expectationname in compiled) {
+                    if(compiled[expectationname].constructor !== expectationtype && !(compiled[expectationname] instanceof expectationtype)) {
+                        throw new Error('Failed expectation for ' + expectationname + '; type mismatch; expected ' + expectationtype.name + '; got ' + compiled[expectationname].constructor.name);
+                    }
                 }
             });
 
-            this.init.apply(compiled);
+            this.init.apply(compiled, [buildprops]);
             return compiled;
         },
         compose: function(...factoriez) {
